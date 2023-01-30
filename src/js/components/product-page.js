a45eb76c3840d36423160eff78e4e3ab43d396e1
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-01-30 20:22:38 +0300

    Улучшает страницу продукта

--- src/js/components/product-page.js
+++ src/js/components/product-page.js
@@ -1,14 +1,18 @@
-if (document.querySelector('.product-page-seciton')) {
-    document.querySelector('.product-page-seciton').addEventListener('click', function(e) {
-        if ((e.target.closest('.product-page--views--element') || e.target.classList.contains('product-page--views--element'))) {
-            e.target.closest('.product-page-seciton').querySelector('.product-page--views--element._active').classList.remove('_active');
-            !e.target.closest('.product-page--views--element') ? e.target.classList.add('_active') : e.target.closest('.product-page--views--element').classList.add('_active');
-            [...document.querySelectorAll('.product-page--views--element')].filter((el, index) => {
-                if (el.classList.contains('_active')) {
-                    document.querySelector('.product-page--views--block-text-descr._active').classList.remove('_active');
-                    document.querySelectorAll('.product-page--views--block-text-descr')[index].classList.add('_active');
-                }
-            });
-        }
+(() => {
+    if (!document.querySelector('.product-page-seciton')) return
+
+    const productVariations = [...document.querySelectorAll('.product-page--views--element')];
+    const productVariationsPic = [...document.querySelectorAll('.product-page__inner-pic')];
+    productVariations.forEach((variation, index) => {
+        variation.addEventListener('click', (e) => {
+            productVariations.forEach((variation) => {
+                variation.classList.remove('_active');
+            })
+            productVariationsPic.forEach((pic) => {
+                pic.classList.remove('_active')
+            })
+            variation.classList.add('_active')
+            productVariationsPic[index].classList.add('_active');
+        });
     });
-}
+})();
